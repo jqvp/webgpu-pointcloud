ca40a44360c8dbe8da26e4b3853ce2de5d4651bf
Joaquín Villamonte Pereira <125866766+jqvp@users.noreply.github.com>
2024-05-23 21:28:59 +0100

    http server

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+const fs = require('fs');
+
+var app = express();
+
+require('dotenv').config();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use("/pkg", express.static('pkg'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log(`Your app is listening on http://localhost:${listener.address().port}/`);
+});
