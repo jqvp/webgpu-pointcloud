7231d1ca1822aa0afbfbcd4b2c7e95bfcb0a4cdc
Joaquín Villamonte Pereira <125866766+jqvp@users.noreply.github.com>
2025-05-18 15:54:05 +0100

    build: print failed file

--- build.rs
+++ build.rs
@@ -5,10 +5,14 @@ fn main() {
     println!("cargo::rerun-if-changed=src/shaders");
     fs::read_dir("src/shaders/").expect("src/shaders/ folder required")
         .filter(|x| x.as_ref().unwrap().metadata().unwrap().is_file())
-        .map(|x| fs::read_to_string(x.unwrap().path()).unwrap())
-        .for_each(|content| {        
+        .map(|x| (
+                x.as_ref().unwrap().path().to_str().unwrap().to_owned(), 
+                fs::read_to_string(x.unwrap().path()).unwrap()
+            )
+        )
+        .for_each(|(path, content)| {        
             if let Err(e) = wgsl::parse_str(&content) {
-                panic!("{}", e);
+                panic!("Error @{}: {}",  path, e);
             }
         });
     
